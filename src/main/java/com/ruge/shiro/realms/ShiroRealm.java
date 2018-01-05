e25409150ac7fde75852d2b8906726b79fa71bb3
13298899809 <wujian1345528755>
2018-01-05 17:10:27 +0800

    shiro demo 01

---
+++ src/main/java/com/ruge/shiro/realms/ShiroRealm.java
@@ -0,0 +1,25 @@
+package com.ruge.shiro.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author 爱丽丝、如歌
+ * @Description: TODO
+ * @date 2018/1/5 13:14
+ */
+public class ShiroRealm  extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
